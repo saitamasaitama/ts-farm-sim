c26ac9be492e236ad59b5d4c0b0d36bd5d15c5e7
shinpei_yoshihara <s-yoshihara@saitama-tec.co.jp>
2019-11-29 09:09:46 +0900

    main.jsは追跡しないように変更 vue.jsで動く用カスタム

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+import * as fs from 'fs'
+import express from 'express' 
+import config from './setting.json'
 
 
+const port = 3002;
+/**
+ * デバッグ用
+ */
+function dd($obj:any){
+  console.dir($obj);
+
+}
+const app = express();
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/', (req:any, res:any)=>{
+   return res.render('index',{
+   })
+});
+
+app.listen(port,() => console.log(`ポート${port}番で処理開始`));
+
+dd(config);
 
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
-import Vue from 'vue'
+//import Vue from 'vue'
+import $ from 'jquery'
+import Vue,{VNode} from 'vue'
 import Component from 'vue-class-component'
+Vue.config.productionTip = false;
 
 @Component({
   template: '<button @click="onClick">Click!</button>'
 })
-
-
-export default class MyComponent extends Vue {
-  message: string = 'Hello!'
-
+class A3 extends Vue {
+  // 初期データはインスタンスプロパティとして宣言できます
+  message: string = 'Hello2';
   onClick (): void {
     window.alert(this.message)
   }
 }
+
+$().ready(()=>{
+  new Vue({
+    render:(h):VNode=>h(A3)
+    }).$mount("#vue2");
+});
